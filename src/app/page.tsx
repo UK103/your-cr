a756a92563456eec4686d7f45a4e1f5bed0a3987
umar-khalid <umarkhalid103@gmail.com>
2024-09-01 11:03:32 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,23 @@ import React from 'react'
 
 const page = () => {
   return (
-      <div><figure className="md:flex bg-blue-300 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-      <img className="w-60 h-90 rounded-full mx-auto" src="/Screenshot (8).png" alt="" width="384" height="512"/>
-      <div className="pt-10 md:p-12 text-center md:text-left space-y-10">
+      <div><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+      <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/Screenshot (8).png" alt="" width="384" height="512"/>
+      <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
         <blockquote>
           <p className="text-lg font-medium">
-            "Meet me I am Choudary Umar Khalid. Artificial Intelligence refers to the intelligence of machines. This is in contrast to the natural intelligence of humans and animals. With Artificial Intelligence, machines perform functions such as learning, planning, reasoning and problem-solving.Artificial Intelligence or AI is a revolutionary technology that allows machines to undertake tasks that usually need human intelligence. It includes natural language processing (NLP), problem-solving capabilities, and machine learning (ML). In contemporary times, AI technology has entered all aspects of human life."        
+            “We have provided here essays and paragraphs on my self in English which is a very common topic in schools. These essays and paragraphs are of variant word limits will help you in writing my self essay, my self paragraph, essay about my self, my self article or myself essay for kids etc.
+
+These essays are written in a very simple language and easy to understand by the children. You can choose any of the essay on myself according to your requirements and earn the app”
           </p>
         </blockquote>
         <figcaption className="font-medium">
           <div className="text-sky-500 dark:text-sky-400">
-            CHOUDARY UMAR KHALID
+            Umar Khalid
           </div>
           <div className="text-slate-700 dark:text-slate-500">
-           Artificial Intelligence Engineer, Rahim Yar Khan
+            ICS student
+            
           </div>
         </figcaption>
       </div>
